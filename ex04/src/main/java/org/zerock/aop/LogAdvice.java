a812143355644804b62a63f918eaaac50e5dc561
hans <hansung0com@gmail.com>
2024-05-18 08:23:37 +0900

    P456

--- ex04/src/main/java/org/zerock/aop/LogAdvice.java
+++ ex04/src/main/java/org/zerock/aop/LogAdvice.java
@@ -11,7 +11,7 @@ import lombok.extern.log4j.Log4j;
 @Component // 스프링에서 빈으로 인식하기 위해 사용
 public class LogAdvice {
 	
-	@Before("execution(* org.zerock.service.SampleService*.*(..)")
+	@Before( "execution(* org.zerock.service.SampleService*.*(..))")
 	//맨앞의 *는 접근제한자 맨뒤의 *.* 는 클래스이름과 메서드이름을 의미
 	public void logBefore() {
 		
