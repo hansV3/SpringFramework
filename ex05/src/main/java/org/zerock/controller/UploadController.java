564ea5a3ed709d9c31f78dc271206982ec0e8ab3
hans <hansung0com@gmail.com>
2024-05-20 09:13:15 +0900

    P511

--- ex05/src/main/java/org/zerock/controller/UploadController.java
+++ ex05/src/main/java/org/zerock/controller/UploadController.java
@@ -1,6 +1,9 @@
 package org.zerock.controller;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -50,12 +53,35 @@ public class UploadController {
 	}
 	
 	
+	private String getFolder() {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		
+		Date date = new Date();
+		
+		String str = sdf.format(date);
+		
+		return str.replace("_", File.separator);
+		
+	}
+	
 	@PostMapping("/uploadAjaxAction")
 	public void uploadAjaxPost(MultipartFile[] uploadFile) {
 		
 		log.info("update ajax post........");
 		
 		String uploadFolder = "C:\\upload";
+		
+		
+		//make folder ............
+		File uploadPath = new File(uploadFolder, getFolder());
+		log.info("upload path: " + uploadPath);
+		
+		if(uploadPath.exists() == false) {
+			uploadPath.mkdirs();
+		}
+		//make yyyy/MM/dd folder
+		
 		
 		for(MultipartFile multipartFile : uploadFile) {
 			
@@ -70,7 +96,12 @@ public class UploadController {
 			
 			log.info("only file name: " + uploadFileName);
 			
-			File saveFile = new File(uploadFolder, uploadFileName);
+			UUID uuid = UUID.randomUUID();
+			
+			uploadFileName = uuid.toString() + "_" + uploadFileName;
+			
+			//File saveFile = new File(uploadFolder, uploadFileName);
+			File saveFile = new File(uploadPath, uploadFileName);
 			
 			try {
 				multipartFile.transferTo(saveFile);
