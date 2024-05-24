fe8627ef26aa9d2f43c372ef9dc426ee7acbd0cc
hans <hansung0com@gmail.com>
2024-05-24 11:46:17 +0900

    P675

--- ex06/src/main/java/org/zerock/security/CustomUserDetailsService.java
+++ ex06/src/main/java/org/zerock/security/CustomUserDetailsService.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.zerock.domain.MemberVO;
 import org.zerock.mapper.MemberMapper;
+import org.zerock.security.domain.CustomUser;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -19,7 +21,13 @@ public class CustomUserDetailsService implements UserDetailsService {
 	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
 
 		log.warn("Load User By userName : " + userName);
-		return null;
+		
+		//userName means userid
+		MemberVO vo = memberMapper.read(userName);
+		
+		log.warn("queried by member mapper: " + vo);
+		
+		return vo == null ? null : new CustomUser(vo);
 	}
 	
 	
